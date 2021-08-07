df5ffa2bc4a44c8d455f0cc5a78cfd45a8967143
greg <40550492+oghupeng@users.noreply.github.com>
2021-08-07 13:58:14 -0500

    Update hupeng.py
    cleanup

--- hupeng.py
+++ hupeng.py
@@ -2,7 +2,7 @@ import smartpy as sp
 
 FA2 = sp.import_script_from_url("https://smartpy.io/dev/templates/FA2.py")
 
-class Bento(FA2.FA2):
+class Hupeng(FA2.FA2):
 
     @sp.entry_point
     def mint(self, params):
@@ -28,35 +28,40 @@ class Bento(FA2.FA2):
             )
         
 
-@sp.add_test(name = "Bento")
+@sp.add_test(name = "Hupeng")
 def test():
     scenario = sp.test_scenario()
     
     admin = sp.test_account("Admin")
     
-    corehupeng = sp.test_account("Core Hupeng")
+    core_hupeng = sp.test_account("Core Hupeng")
     hupeng_alternate = sp.test_account("Hupeng Alternate")
     
-    bento = Bento(FA2.FA2_config(non_fungible = True), admin = admin.address, metadata = sp.big_map({"": sp.bytes_of_string("tezos-storage:content"),"content": sp.bytes_of_string("""{"name" : "Bento"}""")}))
-    scenario += bento
+    hupeng = Hupeng(FA2.FA2_config(non_fungible = True), admin = admin.address, metadata = sp.big_map({"": sp.bytes_of_string("tezos-storage:content"),"content": sp.bytes_of_string("""{"name" : "Hupeng"}""")}))
+    scenario += hupeng
     
     
-    scenario += bento.mint(address = corehupeng.address, 
+    scenario += hupeng.mint(address = core_hupeng.address, 
                             amount = 1,
-                            metadata= Bento.make_metadata(
+                            metadata= Hupeng.make_metadata(
                                 decimals = 0,
-                                name = "Mark bot",
-                                symbol = "CTB"
+                                name = "Core Hupeng",
+                                symbol = "CHP"
                             ),
                             token_id= 0
                             ).run(sender = admin)
 
     
-    scenario += bento.mint(address = hupeng_alternate.address,
+    scenario += hupeng.mint(address = hupeng_alternate.address,
                             amount = 1,
-                            metadata = Bento.make_metadata(
+                            metadata = Hupeng.make_metadata(
                                 decimals = 0,
-                                name = "Elon bot",
-                                symbol = "CTB"
+                                name = "Hupeng Alternate",
+                                symbol = "AHP"
                             ),
                             token_id = 1).run(sender = admin)
+                            
+    # (1) Create a test account operator with the seed of "operator".              
+    
+    
+    # (2) Call entry point update_operator and give operator the access to transfer token with token_id 0 for mark.
